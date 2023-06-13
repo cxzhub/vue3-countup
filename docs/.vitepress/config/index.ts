835e376d5576a54e203e97f5d5e541aff86c172c
chenxingzhi <chen.xingzhi@beicaizs.com>
2023-06-13 17:59:51 +0800

    chore: 修改docs

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -4,11 +4,22 @@ import {
 } from '@vitepress-demo-preview/plugin'
 
 module.exports = {
-  title: 'Vue3Countup',
+  title: 'Vue3 Countup',
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/cxzhub/vue3-countup' }
+    ],
+
+    footer: {
+      message: `Released under the MIT License.`,
+      copyright: 'Copyright © 2023-present Chen Xing zhi'
+    }
+  },
   markdown: {
     config(md) {
       md.use(containerPreview)
       md.use(componentPreview)
     }
-  }
+  },
+  base: '/vue3-countup/'
 }
